90712b7f8050254e65a9c16519d14ad6c750bad2
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2025-02-06 16:36:52 +0100

    Updated the codes to the game

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+// GETTING ELEMENTS
+
+// GETTING THE TOTAL SCORE
+let score = document.querySelector(".score");
+
+
+// GETTING THE ROCK PAPER SCISSORS OPTIONS
+let paper = document.querySelector(".paper");
+let scissors = document.querySelector(".scissors");
+let rock = document.querySelector(".rock");
+
+
+// SELECTING CHOICES ELEMENTS
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+let score = document.querySelector(".score");
+
+score.textContent = 13;
