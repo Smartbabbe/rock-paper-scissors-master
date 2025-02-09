daf9a906f9e52f0008bcf04f476e891d173ce665
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2025-02-09 10:48:28 +0100

    worked on the js

--- index.js
+++ index.js
@@ -11,9 +11,26 @@ let score = document.querySelector(".score");
 let paper = document.querySelector(".paper");
 let scissors = document.querySelector(".scissors");
 let rock = document.querySelector(".rock");
+let options = document.querySelector(".options")
 
 
 // SELECTING CHOICES ELEMENTS
+let myChoiceDiv = document.querySelector(".mychoice");
+let myChoiceImg = document.querySelector(".yourchoice");
+let computerChoiceDiv = document.querySelector(".computerchoice");
+let computerChoiceImg = document.querySelector(".computerChoice");
 
+
+function addHidden () {
+    options.classList.add("hidden");
+};
+
+
+// FUNCTION TO ADD HIDDEN CLASS
+
+let clickArray = [paper, scissors, rock];
+clickArray.forEach(element => {
+    element.addEventListener("click", addHidden);  
+});
 
 score.textContent = 13;
